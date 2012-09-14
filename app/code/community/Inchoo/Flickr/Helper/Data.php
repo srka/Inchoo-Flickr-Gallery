6bae6dbf7ba8c52cf49535fa72cb875227651b3e
Srdjan Stojiljkovic <srdjan@inchoo.net>
2012-09-14 16:22:40 +0200

    Removing lowercase files and folders

--- app/code/community/Inchoo/Flickr/Helper/Data.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Inchoo_Flickr_Helper_Data extends Mage_Core_Helper_Abstract{
-	
-}
--- app/code/community/Inchoo/Flickr/Model/flickr.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Inchoo_Flickr_Model_Flickr extends Mage_Core_Model_Abstract{
-	public function _construct(){
-		parent::_construct();
-		$this->_init('flickr/flickr');
-	}
-}
